d2085f970d54f21895fdff24d86419954ed5333d
Mark Lawson <mlawson3691@gmail.com>
2016-08-12 08:31:58 -0700

    add jquery to cycle through and display each question when the button is clicked, and display the result at the end

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(function() {
 
-  
+  var questionCounter = 1;
+
+  $("#submit").click(function(event) {
+    $('#question' + questionCounter).hide();
+    if (questionCounter < 5) {
+      questionCounter += 1;
+      $('#question' + questionCounter).fadeIn();
+    } else {
+      $("#submit").hide();
+      $("#result").show();
+    }
+
+    event.preventDefault();
+  });
+
 });
