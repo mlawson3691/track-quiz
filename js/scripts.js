f817d91ac3ce73a0edd2bee9e88acd31d259194a
Mark Lawson <mlawson3691@gmail.com>
2016-08-12 15:30:01 -0700

    refactor scripts.js to condense code into separate functions

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,88 @@ $(function() {
   var design = 0;
   var net = 0;
 
+  // Based on the current question, increase the user's score for each language based on their answers
+  var score = function(question) {
+    if (question === 1) {
+      if ($("input:radio[name=question1]:checked").val() === "1") {
+        design += 3;
+      } else {
+        rails += 1;
+        drupal += 1;
+        android += 1;
+        net += 1;
+      }
+    } else if (question === 2) {
+      if ($("input:radio[name=question2]:checked").val() === "1") {
+        drupal += 1;
+      } else {
+        rails += 1;
+      }
+    } else if (question === 3) {
+      if ($("input:radio[name=question3]:checked").val() === "1") {
+        rails += 1;
+        drupal += 1;
+        net += 1;
+        design += 1;
+      } else {
+        android += 3;
+      }
+    } else if (question === 4) {
+      if ($("input:radio[name=question4]:checked").val() === "1") {
+        net += 2;
+      } else {
+        rails += 1;
+        drupal += 1;
+      }
+    } else if (question === 5) {
+      if ($("input:radio[name=question5]:checked").val() === "1") {
+        drupal += 1;
+        rails += 1;
+        design += 1;
+      } else {
+        net += 1;
+        android += 1
+      }
+    }
+  };
+
+  // Evaluates and displays the appropriate language, links, and icons based on user's score
+  var evaluate = function() {
+
+    var language, trackLink;
+
+    if (rails >= drupal && rails >= android && rails >= design && rails >= net) {
+      language = "Ruby & Rails";
+      $("#icon1").addClass("devicon-ruby-plain");
+      $("#icon2").addClass("devicon-rails-plain");
+      trackLink = "<a href='http://www.epicodus.com/ruby' target='_blank'>" + language + "</a>";
+    } else if (drupal >= rails && drupal >= android && drupal >= design && drupal >= net) {
+      language = "PHP & Drupal";
+      $("#icon1").addClass("devicon-php-plain");
+      $("#icon2").addClass("devicon-drupal-plain");
+      trackLink = "<a href='http://www.epicodus.com/php' target='_blank'>" + language + "</a>";
+    } else if (android >= rails && android >= drupal && android >= design && android >= net) {
+      language = "Java & Android";
+      $("#icon1").addClass("devicon-java-plain");
+      $("#icon2").addClass("devicon-android-plain");
+      trackLink = "<a href='http://www.epicodus.com/java' target='_blank'>" + language + "</a>";
+    } else if (design >= rails && design >= android && design >= drupal && design >= net) {
+      language = "CSS & Design";
+      $("#icon1").addClass("devicon-css3-plain");
+      $("#icon2").hide();
+      trackLink = "<a href='http://www.epicodus.com/css' target='_blank'>" + language + "</a>";
+    } else if (net >= rails && net >= android && net >= drupal && net >= design) {
+      language = "C# & .NET";
+      $("#icon1").addClass("devicon-csharp-plain");
+      $("#icon2").addClass("devicon-windows8-original");
+      trackLink = "<a href='http://www.epicodus.com/c-sharp' target='_blank'>" + language + "</a>";
+    }
+
+    $("#trackLink").html(trackLink);
+    $("#language").text(language);
+  };
+
+  // Next Button
   $("#submit").click(function(event) {
     $('.alert').hide();
 
@@ -17,48 +99,8 @@ $(function() {
       $("#nameForm").hide();
       $("#question1").fadeIn();
     } else if ($("input:radio[name=question" + questionCounter + "]:checked").val()) { // subsequent successful button clicks
-      // Based on the current question, increase the user's score for each language based on their answers
-      if (questionCounter === 1) {
-        if ($("input:radio[name=question1]:checked").val() === "1") {
-          design += 3;
-        } else {
-          rails += 1;
-          drupal += 1;
-          android += 1;
-          net += 1;
-        }
-      } else if (questionCounter === 2) {
-        if ($("input:radio[name=question2]:checked").val() === "1") {
-          drupal += 1;
-        } else {
-          rails += 1;
-        }
-      } else if (questionCounter === 3) {
-        if ($("input:radio[name=question3]:checked").val() === "1") {
-          rails += 1;
-          drupal += 1;
-          net += 1;
-          design += 1;
-        } else {
-          android += 3;
-        }
-      } else if (questionCounter === 4) {
-        if ($("input:radio[name=question4]:checked").val() === "1") {
-          net += 2;
-        } else {
-          rails += 1;
-          drupal += 1;
-        }
-      } else if (questionCounter === 5) {
-        if ($("input:radio[name=question5]:checked").val() === "1") {
-          drupal += 1;
-          rails += 1;
-          design += 1;
-        } else {
-          net += 1;
-          android += 1
-        }
-      }
+
+      score(questionCounter);
 
       // Hide the current question and show either the next one or the final result
       $('#question' + questionCounter).hide();
@@ -68,36 +110,8 @@ $(function() {
       } else {
         $("#submit").hide();
 
-        var language, trackLink;
+        evaluate();
 
-        if (rails >= drupal && rails >= android && rails >= design && rails >= net) {
-          language = "Ruby & Rails";
-          $("#icon1").addClass("devicon-ruby-plain");
-          $("#icon2").addClass("devicon-rails-plain");
-          trackLink = "<a href='http://www.epicodus.com/ruby' target='_blank'>" + language + "</a>";
-        } else if (drupal >= rails && drupal >= android && drupal >= design && drupal >= net) {
-          language = "PHP & Drupal";
-          $("#icon1").addClass("devicon-php-plain");
-          $("#icon2").addClass("devicon-drupal-plain");
-          trackLink = "<a href='http://www.epicodus.com/php' target='_blank'>" + language + "</a>";
-        } else if (android >= rails && android >= drupal && android >= design && android >= net) {
-          language = "Java & Android";
-          $("#icon1").addClass("devicon-java-plain");
-          $("#icon2").addClass("devicon-android-plain");
-          trackLink = "<a href='http://www.epicodus.com/java' target='_blank'>" + language + "</a>";
-        } else if (design >= rails && design >= android && design >= drupal && design >= net) {
-          language = "CSS & Design";
-          $("#icon1").addClass("devicon-css3-plain");
-          $("#icon2").hide();
-          trackLink = "<a href='http://www.epicodus.com/css' target='_blank'>" + language + "</a>";
-        } else if (net >= rails && net >= android && net >= drupal && net >= design) {
-          language = "C# & .NET";
-          $("#icon1").addClass("devicon-csharp-plain");
-          $("#icon2").addClass("devicon-windows8-original");
-          trackLink = "<a href='http://www.epicodus.com/c-sharp' target='_blank'>" + language + "</a>";
-        }
-        $("#trackLink").html(trackLink);
-        $("#language").text(language);
         $("#result").fadeIn();
         $("#reset").fadeIn();
       }
@@ -108,8 +122,8 @@ $(function() {
     event.preventDefault();
   });
 
+  // "Take it Again" Button
   $("#reset").click(function(){
     location.reload();
   });
-
 });
